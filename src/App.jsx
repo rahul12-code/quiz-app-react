81c9f042ba68d1d9cfdb333f312717f8d4db8b9e
rahul <rahulkandati@gmail.com>
2024-12-21 12:29:46 +0530

    initialized react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-[100px] text-red-800 italic'>
+      QUIZ APP REACT 
+    </div>
+  )
+}
+
+export default App
